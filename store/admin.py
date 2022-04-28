e5e63261b96aec0a4b85ee0677d3e7c3211fc0ab
daxazz1 <90633516+daxazz1@users.noreply.github.com>
2022-04-29 01:53:27 +0300

    ok

---
+++ store/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models.product import Products
+from .models.category import Category
+from .models.customer import Customer
+from .models.orders import Order
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['first_name']
+
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['product']
+
+
+# Register your models here.
+admin.site.register(Products, AdminProduct)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Customer, AdminCustomer)
+admin.site.register(Order, AdminOrder)
---
+++ store/models/category.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
+
+    def __str__(self):
+        return self.name
