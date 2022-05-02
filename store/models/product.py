0e29c074f9da4aa653c9af9e86d11f4d0da6c834
daxazz1 <90633516+daxazz1@users.noreply.github.com>
2022-05-02 13:19:44 +0300

    ok

--- store/models/product.py
+++ store/models/product.py
@@ -4,10 +4,10 @@ from .category import Category
 
 class Products(models.Model):
     name = models.CharField(max_length=60)
-    price= models.IntegerField(default=0)
-    category= models.ForeignKey(Category,on_delete=models.CASCADE,default=1 )
-    description= models.CharField(max_length=250, default='', blank=True, null= True)
-    image= models.ImageField(upload_to='uploads/products/')
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/')
 
     def __str__(self):
         return self.name
@@ -18,7 +18,8 @@ class Products(models.Model):
 
     @staticmethod
     def get_products_by_id(ids):
-        return Products.objects.filter (id__in=ids)
+        return Products.objects.filter(id__in=ids)
+
     @staticmethod
     def get_all_products():
         return Products.objects.all()
@@ -26,6 +27,6 @@ class Products(models.Model):
     @staticmethod
     def get_all_products_by_categoryid(category_id):
         if category_id:
-            return Products.objects.filter (category=category_id)
+            return Products.objects.filter(category=category_id)
         else:
             return Products.get_all_products();
